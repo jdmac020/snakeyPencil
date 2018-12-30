472d2403590416abbb69b5a732e2c2202c6b5fa5
jdmac020 <john.macgregor@gmail.com>
2018-12-30 11:29:51 -0500

    Refactored test class

--- paper_tests.py
+++ paper_tests.py
@@ -4,27 +4,23 @@ from writing_set.paper import Sheet
 class PaperTests(unittest.TestCase):
 
 	def setUp(self):
-		pass
+		self.string1 = "Hello you"
+		self.string2 = "Bilbo Baggins"
+		self.combinedString = self.string1 + self.string2
+		self.sheet = Sheet()
 		
 	def test_whenWritingString_contentIsInput(self):
 	
-		testString = "Hello you"
-		testPaper = Sheet()
-		
-		testPaper.write_down(testString)
+		self.sheet.write_down(self.string1)
 	
-		self.assertEqual( testPaper.get_content(), testString)
+		self.assertEqual( self.sheet.get_content(), self.string1)
 		
 	def test_whenWritingTwice_secondStringAppendedToFirst(self):
 	    
-		string1 = "Hello you"
-		string2 = " Bilbo Baggins"
-		testPaper = Sheet()
+		self.sheet.write_down(self.string1)
+		self.sheet.write_down(self.string2)
 		
-		testPaper.write_down(string1)
-		testPaper.write_down(string2)
-		
-		self.assertEqual( testPaper.get_content(), string1+string2)
+		self.assertEqual( self.sheet.get_content(), self.combinedString)
 		
 		
 if __name__ == "__main__":
